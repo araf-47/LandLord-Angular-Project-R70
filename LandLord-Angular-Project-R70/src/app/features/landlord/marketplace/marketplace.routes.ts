5a68b74050c18e663c874abfecc6d06c2d02c199
Araf47.edu <araf47.edu@gmail.com>
2026-08-07 01:00:16 +0600

    added a manual payment entry record by landlord for the tenants

--- LandLord-Angular-Project-R70/src/app/features/landlord/marketplace/marketplace.routes.ts
+++ LandLord-Angular-Project-R70/src/app/features/landlord/marketplace/marketplace.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const MARKETPLACE_ROUTES: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'ads' },
-  { path: 'ads', loadComponent: () => import('./ad-management.component').then((m) => m.AdManagementComponent) },
-  { path: 'requests', loadComponent: () => import('./request-list.component').then((m) => m.RequestListComponent) },
-  { path: 'requests/:requestId', loadComponent: () => import('./request-detail.component').then((m) => m.RequestDetailComponent) },
+  {
+    path: '',
+    loadComponent: () => import('./marketplace-layout.component').then((m) => m.MarketplaceLayoutComponent),
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'ads' },
+      { path: 'ads', loadComponent: () => import('./ad-management.component').then((m) => m.AdManagementComponent) },
+      { path: 'requests', loadComponent: () => import('./request-list.component').then((m) => m.RequestListComponent) },
+      { path: 'requests/:requestId', loadComponent: () => import('./request-detail.component').then((m) => m.RequestDetailComponent) },
+    ],
+  },
 ];
